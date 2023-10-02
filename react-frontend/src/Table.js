62c5b586697b6a77cf96a20b2391a717458d6e59
Josh <jqueja@calpoly.edu>
2023-10-02 00:56:32 -0700

    Just finished with part 4 of the react part 2 assignment

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -16,9 +16,15 @@ function TableHeader() {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
+	  <td>{row.name}</td>
+	  <td>{row.job}</td>
+	  <td>
+			<button onClick={() => 
+				props.removeCharacter(index)}>
+				Delete
+			</button>
+	  </td>
+	</tr>
       );
      }
     );
@@ -29,13 +35,14 @@ function TableHeader() {
      );
   }
 
-  function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} 
+	      removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 
 export default Table;
