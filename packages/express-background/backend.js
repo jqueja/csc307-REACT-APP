328504ab84e1833de8e914467ab7984b6d438729
Josh <jqueja@calpoly.edu>
2023-10-08 23:38:06 -0700

    implemented delete method

--- packages/express-background/backend.js
+++ packages/express-background/backend.js
@@ -47,6 +47,20 @@ const findUserById = (id) =>
     return user;
 }
 
+// Function to delete a user by ID
+const deleteUserById = (id) => {
+   // Find the index of the user with the specified ID
+   const userIndex = users.users_list.findIndex((user) => user.id === id);
+
+   // If the user is found, remove it from the array
+   if (userIndex !== -1) {
+       users.users_list.splice(userIndex, 1);
+       return true; // Indicate success
+   }
+
+   return false; // Indicate failure (user not found)
+};
+
 app.use(express.json());
 
 app.get('', (req, res) => {
@@ -76,11 +90,25 @@ app.get('/users', (req, res) => {
 app.get('/users/:id', (req, res) => {
     const id = req.params['id']; //or req.params.id
     let result = findUserById(id);
+
     if (result === undefined) {
         res.status(404).send('Resource not found.');
     } else {
         res.send(result);
     }
+});
+
+app.delete('/users/:id', (req, res) => {
+   const id = req.params['id']
+
+   const success = deleteUserById(id)
+
+
+   if (success) {
+       res.send('User deleted')
+   } else {
+      res.status(404).send('User not Found')
+   }
 });
 
 app.post('/users', (req, res) => {
